5931d6668e660af7bde44c6373360911ed9c183d
Larysmith <ayinla.olanrewaju@gmail.com>
2022-11-28 11:28:35 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for State class."""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class representing a State."""
+    name = ""
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
